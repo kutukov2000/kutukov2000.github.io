9643bfcb75a993e164c9d471fdaa10a475eb2b68
kutukov2000 <73742110+kutukov2000@users.noreply.github.com>
2020-12-03 18:42:03 +0200

    Add files via upload

---
+++ A2.Site/script/ararat.js
@@ -0,0 +1,28 @@
+ //Artur`s scripts
+
+// Katalog images
+let k_img = document.querySelectorAll('.katalog_image');
+// katalog text "Детальніше"
+let k_p = document.querySelectorAll(".katalog_button");
+//Katalog div
+let container = document.querySelectorAll('.container')
+
+for(let i=0; i<container.length;i++){
+
+    container[i].addEventListener('mouseenter',function(){
+        k_img[i].style.filter = "blur(5px) brightness(0.55)"
+        k_p[i].style.display = "block"
+        k_p[i].style.opacity = "1"
+        k_p[i].style.border = "2px solid rgba(255, 255, 255, 1)"
+
+    })
+
+    container[i].addEventListener('mouseleave',function(){
+        k_img[i].style.filter = ""
+        k_p[i].style.opacity = "0"
+        k_p[i].style.border = "2px solid rgba(255, 255, 255, 0)"
+        k_p[i].style.display = "none"
+
+
+    })
+}
